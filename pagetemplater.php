ee0c14f4906cf7a0427e4099c6f44f6d5754cb42
Cristian Marin <tian.marin@gmail.com>
2017-02-08 16:25:12 -0600

    PageTemplater Fix
    PAgetemplater fix for WP 4.7

--- pagetemplater.php
+++ pagetemplater.php
@@ -1,142 +1,165 @@
 <?php
-	class CSI_PageTemplater {
+/*
+Plugin Name: Page Template Plugin : 'Good To Be Bad'
+Plugin URI: http://www.wpexplorer.com/wordpress-page-templates-plugin/
+Version: 1.1.0
+Author: WPExplorer
+Author URI: http://www.wpexplorer.com/
+*/
 
-		/**
-         * A Unique Identifier
-         */
-		 protected $plugin_slug;
+class CSI_PageTemplater {
 
-        /**
-         * A reference to an instance of this class.
-         */
-        private static $instance;
+	/**
+	 * A reference to an instance of this class.
+	 */
+	private static $instance;
 
-        /**
-         * The array of templates that this plugin tracks.
-         */
-        protected $templates;
+	/**
+	 * The array of templates that this plugin tracks.
+	 */
+	protected $templates;
 
+	/**
+	 * Returns an instance of this class. 
+	 */
+	public static function get_instance() {
 
-        /**
-         * Returns an instance of this class. 
-         */
-        public static function get_instance() {
+		if ( null == self::$instance ) {
+			self::$instance = new CSI_PageTemplater();
+		} 
 
-                if( null == self::$instance ) {
-                        self::$instance = new CSI_PageTemplater();
-                } 
+		return self::$instance;
 
-                return self::$instance;
+	} 
 
-        } 
+	/**
+	 * Initializes the plugin by setting filters and administration functions.
+	 */
+	private function __construct() {
 
-        /**
-         * Initializes the plugin by setting filters and administration functions.
-         */
-        private function __construct() {
-
-                $this->templates = array();
-
-
-                // Add a filter to the attributes metabox to inject template into the cache.
-                add_filter(
-					'page_attributes_dropdown_pages_args',
-					 array( $this, 'register_project_templates' ) 
-				);
+		$this->templates = array();
 
 
-                // Add a filter to the save post to inject out template into the page cache
-                add_filter(
-					'wp_insert_post_data', 
-					array( $this, 'register_project_templates' ) 
-				);
+		// Add a filter to the attributes metabox to inject template into the cache.
+		if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) {
+
+			// 4.6 and older
+			add_filter(
+				'page_attributes_dropdown_pages_args',
+				array( $this, 'register_project_templates' )
+			);
+
+		} else {
+
+			// Add a filter to the wp 4.7 version attributes metabox
+			add_filter(
+				'theme_page_templates', array( $this, 'add_new_template' )
+			);
+
+		}
+
+		// Add a filter to the save post to inject out template into the page cache
+		add_filter(
+			'wp_insert_post_data', 
+			array( $this, 'register_project_templates' ) 
+		);
 
 
-                // Add a filter to the template include to determine if the page has our 
-				// template assigned and return it's path
-                add_filter(
-					'template_include', 
-					array( $this, 'view_project_template') 
-				);
+		// Add a filter to the template include to determine if the page has our 
+		// template assigned and return it's path
+		add_filter(
+			'template_include', 
+			array( $this, 'view_project_template') 
+		);
 
 
-                // Add your templates to this array.
-                $this->templates = array(
-                	'templates/template-project-request.php'			=> 'CSI Project Request',
-					'templates/template-project-request-status.php'		=> 'CSI Project Request Status',
-					'templates/template-cmp-planner.php'				=> 'CSI PCM Planner',
-					'templates/template-ewa-upload.php'					=> 'CSI EWA Uploader',
-					'templates/template-ewa-control-center.php'			=> 'CSI EWA Control Center',
-					'templates/template-ewa-mgmt-control-center.php'	=> 'CSI EWA Mgmt Control Center',
-                );
-				
-        } 
+		// Add your templates to this array.
+        $this->templates = array(
+        	'templates/template-project-request.php'			=> 'CSI Project Request',
+			'templates/template-project-request-status.php'		=> 'CSI Project Request Status',
+			'templates/template-cmp-planner.php'				=> 'CSI PCM Planner',
+			'templates/template-ewa-upload.php'					=> 'CSI EWA Uploader',
+			'templates/template-ewa-control-center.php'			=> 'CSI Alert Management',
+			'templates/template-ewa-mgmt-control-center.php'	=> 'CSI Control Center',
+        );
+			
+	} 
 
+	/**
+	 * Adds our template to the page dropdown for v4.7+
+	 *
+	 */
+	public function add_new_template( $posts_templates ) {
+		$posts_templates = array_merge( $posts_templates, $this->templates );
+		return $posts_templates;
+	}
 
-        /**
-         * Adds our template to the pages cache in order to trick WordPress
-         * into thinking the template file exists where it doens't really exist.
-         *
-         */
+	/**
+	 * Adds our template to the pages cache in order to trick WordPress
+	 * into thinking the template file exists where it doens't really exist.
+	 */
+	public function register_project_templates( $atts ) {
 
-        public function register_project_templates( $atts ) {
+		// Create the key used for the themes cache
+		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
 
-                // Create the key used for the themes cache
-                $cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+		// Retrieve the cache list. 
+		// If it doesn't exist, or it's empty prepare an array
+		$templates = wp_get_theme()->get_page_templates();
+		if ( empty( $templates ) ) {
+			$templates = array();
+		} 
 
-                // Retrieve the cache list. 
-				// If it doesn't exist, or it's empty prepare an array
-                $templates = wp_get_theme()->get_page_templates();
-                if ( empty( $templates ) ) {
-                        $templates = array();
-                } 
+		// New cache, therefore remove the old one
+		wp_cache_delete( $cache_key , 'themes');
 
-                // New cache, therefore remove the old one
-                wp_cache_delete( $cache_key , 'themes');
+		// Now add our template to the list of templates by merging our templates
+		// with the existing templates array from the cache.
+		$templates = array_merge( $templates, $this->templates );
 
-                // Now add our template to the list of templates by merging our templates
-                // with the existing templates array from the cache.
-                $templates = array_merge( $templates, $this->templates );
+		// Add the modified cache to allow WordPress to pick it up for listing
+		// available templates
+		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 
-                // Add the modified cache to allow WordPress to pick it up for listing
-                // available templates
-                wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+		return $atts;
 
-                return $atts;
+	} 
 
-        } 
+	/**
+	 * Checks if the template is assigned to the page
+	 */
+	public function view_project_template( $template ) {
+		
+		// Get global post
+		global $post;
 
-        /**
-         * Checks if the template is assigned to the page
-         */
-        public function view_project_template( $template ) {
+		// Return template if post is empty
+		if ( ! $post ) {
+			return $template;
+		}
 
-                global $post;
+		// Return default template if we don't have a custom one defined
+		if ( ! isset( $this->templates[get_post_meta( 
+			$post->ID, '_wp_page_template', true 
+		)] ) ) {
+			return $template;
+		} 
 
-                if (!isset($this->templates[get_post_meta( 
-					$post->ID, '_wp_page_template', true 
-				)] ) ) {
-					
-                        return $template;
-						
-                } 
+		$file = plugin_dir_path( __FILE__ ). get_post_meta( 
+			$post->ID, '_wp_page_template', true
+		);
 
-                $file = plugin_dir_path(__FILE__). get_post_meta( 
-					$post->ID, '_wp_page_template', true 
-				);
-				
-                // Just to be safe, we check if the file exist first
-                if( file_exists( $file ) ) {
-                        return $file;
-                } 
-				else { echo $file; }
+		// Just to be safe, we check if the file exist first
+		if ( file_exists( $file ) ) {
+			return $file;
+		} else {
+			echo $file;
+		}
 
-                return $template;
+		// Return template
+		return $template;
 
-        } 
-
+	}
 
 } 
-
 add_action( 'plugins_loaded', array( 'CSI_PageTemplater', 'get_instance' ) );
-?>
